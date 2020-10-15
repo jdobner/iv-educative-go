a2acc641418450b5b44a8b032f8aed32f679297d
Jerry Dobner <jdobner@gmail.com>
2020-10-15 13:37:25 -0400

    more

--- main2.go
+++ main2.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import f "fmt"
 
 func sayHello() {
-	fmt.Println("Hello Jerry")
+	f.Println("Hello Jerry")
+	doStuff1()
+}
+
+func doStuff1() {
+	i := 0
+	var j bool
+	f.Println(i)
+	f.Println(j)
+	f.Println("")
+	f.Println(isZeroInt(0))
+	f.Println(isZeroInt(9))
+	f.Println(isZeroFloat(9))
+
+	const nine = 9
+	f.Println(isZeroFloat(nine))
+}
+
+func isZeroInt(num int) bool {
+	return num == 0
+}
+
+
+func isZeroFloat(num float32) bool {
+        return num == 0
 }
