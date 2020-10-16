cd6a7fc0ec2abc82f98026324ec7e756997088a0
Jerry Dobner <jdobner@gmail.com>
2020-10-16 10:47:59 -0400

    deleted main2

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
+import f "fmt"
+
+
+type gender int  
+const (
+	UNKNOWN = iota
+	MALE 
+	FEMALE 
+)
+
+func genderInfo(g gender) {
+	f.Println(g)
+}
+
 func main() {
-	sayHello()
+	f.Println("Hello Jerry")
+	doStuff1()
+}
+
+func doStuff1() {
+	i := 0
+	var j bool
+	f.Println(i)
+	f.Println(j)
+	f.Println("")
+	f.Println(isZeroInt(0))
+	f.Println(isZeroInt(9))
+	f.Println(isZeroFloat(9))
+
+	const nine = 9
+	f.Println(isZeroFloat(nine))
+
+	genderInfo(5)
+}
+
+func isZeroInt(num int) bool {
+	return num == 0
 }
 
 
+func isZeroFloat(num float32) bool {
+        return num == 0
+}
